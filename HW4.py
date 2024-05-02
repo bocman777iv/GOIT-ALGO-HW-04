eb1c4de5b49c08936edc16d8c4bcc7ad0b656eb7
Ivan <165039394+bocman777iv@users.noreply.github.com>
2024-05-02 11:46:15 +0300

    Add files via upload

---
+++ HW4.py
@@ -0,0 +1,59 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+def change_contact(args, contacts):
+    if len(args) == 2:
+        name, new_phone_number = args
+        if name in contacts:
+            contacts[name] = new_phone_number
+            return "Contact updated."
+        else:
+            return "Contact not found."
+
+def show_phone(args, contacts):
+    if len(args) == 1:
+        name = args[0]
+        if name in contacts:
+            return contacts[name]
+        else:
+            return "Contact not found."
+
+def show_all(contacts):
+    if contacts:
+        for name, phone_number in contacts.items():
+            print(f"{name} {phone_number}")
+    else:
+        print("No contacts.")
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "phone":
+            print(show_phone(args, contacts))
+        elif command == "all":
+            show_all(contacts)
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
